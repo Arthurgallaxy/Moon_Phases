861be03bba54deeca96e7d100143cc183e790d96
LunaHoenders <l.hoenders@student.maastrichtuniversity.nl>
2026-02-12 14:18:20 +0100

    Update Bodies.py
    4 potential functions for the class of the cat

--- Bodies.py
+++ Bodies.py
@@ -1,4 +1,19 @@
-#This file was created on the 05/02/26.
-print("Hello")
-print("Test")
-print("Margot is testing :)")
+  def __init__(self, latitude, longitude, alive=True, date_time=None):
+        # Initialize the cat's properties
+        self.latitude = latitude
+        self.longitude = longitude
+        self.alive = alive
+        self.date_time = date_time
+
+    def set_position(self, latitude, longitude):
+        # Update the cat's latitude and longitude
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def check_alive(self):
+        # Check if the cat is alive
+        return self.alive
+
+    def set_date_time(self, date_time):
+        # Set or update the observation time
+        self.date_time = date_time
