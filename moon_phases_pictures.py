bd72467ed67a55b1ef0a58fb22c1f875377511a3
LunaHoenders <l.hoenders@student.maastrichtuniversity.nl>
2026-03-11 15:09:45 +0100

    Adding comments
    Added directory path definitions and improved class structure for MoonPhaseCalculator.

--- moon_phases_pictures.py
+++ moon_phases_pictures.py
@@ -3,14 +3,17 @@ from PIL import Image
 from pathlib import Path
 
 dec = decimal.Decimal
+
+#Define directory paths for the image files
 BASE_DIR = Path(__file__).resolve().parent
 IMG_DIR = BASE_DIR / "moon_images"
 
-
+#Create the MoonPhaseCalculator class
 class MoonPhaseCalculator:
     def __init__(self, img_dir):
         self.img_dir = Path(img_dir)
 
+#Return the percentage of lumination
     @staticmethod
     def position(now=None):
         if now is None:
@@ -22,6 +25,7 @@ class MoonPhaseCalculator:
 
         return lunations % dec(1)
 
+#Method for the moon phase
     def phase(self, pos):
         index = (pos * dec(8)) + dec("0.5")
         index = math.floor(index)
@@ -43,10 +47,12 @@ class MoonPhaseCalculator:
 
         return phase_name, image_path
 
+#Converts a string into a daytime object
     @staticmethod
     def parse_date(date_str):
         return datetime.datetime.strptime(date_str, "%Y-%m-%d")
 
+#Final calculation for the moonphase and error handeling
     def moon_phase_for_date(self, date_str):
         try:
             input_date = self.parse_date(date_str)
